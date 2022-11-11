62046dd90507d25c2866eaf532418c3fdc190a0b
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2022-11-11 22:14:48 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="coolwsd code-brand"
+pkg_dependencies="coolwsd code-brand hunspell-fr hunspell"
 
 #=================================================
 # PERSONAL HELPERS
