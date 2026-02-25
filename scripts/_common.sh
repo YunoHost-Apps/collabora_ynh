254658d133ba25203359260ce61430ecacc4591d
Rodolphe Robles <rodolphe.robles.fr>
2026-02-25 17:38:13 +0100

    removed non-free dependencies if needed

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,8 +7,6 @@
 # Activate ttf-mscorefonts
 mscorefonts_activate(){
     ynh_apt_install_dependencies_from_extra_repository --repo="deb http://deb.debian.org/debian bookworm contrib non-free non-free-firmware" --package="ttf-mscorefonts-installer" --key="https://ftp-master.debian.org/keys/archive-key-12.asc"
-    # reload templates
-    coolconfig update-system-template
 
     ynh_print_info "microsoft fonts installed"
 }
@@ -17,7 +15,12 @@ mscorefonts_activate(){
 mscorefonts_deactivate(){
 	apt remove --purge ttf-mscorefonts-installer
 	fc-cache -rv
-#	rm /etc/apt/sources.list.d/
-	# reload templates
-    coolconfig update-system-template
+	ynn_safe_rm /etc/apt/sources.list.d/collabora.list
+
+	ynh_print_info "microsoft fonts removed"
+}
+
+# Reload templates
+coolconfig_update_system_template(){
+	coolconfig update-system-template
 }
