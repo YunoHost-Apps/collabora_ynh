76daf96f48e3e689cd4cda826f41e2b1cf1cdaf5
Rodolphe Robles <rodolphe.robles.fr>
2026-02-25 22:12:39 +0100

    add conditions

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 
 # Activate ttf-mscorefonts
 mscorefonts_activate(){
-    ynh_apt_install_dependencies_from_extra_repository --repo="deb http://deb.debian.org/debian bookworm contrib non-free non-free-firmware" --package="ttf-mscorefonts-installer" --key="https://ftp-master.debian.org/keys/archive-key-12.asc"
+    ynh_apt_install_dependencies_from_extra_repository --repo="deb http://deb.debian.org/debian bookworm contrib non-free-firmware" --package="ttf-mscorefonts-installer" --key="https://ftp-master.debian.org/keys/archive-key-12.asc"
     fc-cache -rv
 
     ynh_print_info "microsoft fonts installed"
@@ -24,4 +24,5 @@ mscorefonts_deactivate(){
 # Reload templates
 coolconfig_update_system_template(){
 	coolconfig update-system-template
+	ynh_systemctl --service="coolwsd" --action="restart" --log_path="systemd" --wait_until="Ready to accept connections"
 }
