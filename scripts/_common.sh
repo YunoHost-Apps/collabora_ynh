e775696215a4ed8d6bf3bbfe59bc399c3751b33e
Rodolphe Robles <rodolphe.robles.fr>
2026-02-25 20:01:27 +0100

    cleaning

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,18 +7,19 @@
 # Activate ttf-mscorefonts
 mscorefonts_activate(){
     ynh_apt_install_dependencies_from_extra_repository --repo="deb http://deb.debian.org/debian bookworm contrib non-free non-free-firmware" --package="ttf-mscorefonts-installer" --key="https://ftp-master.debian.org/keys/archive-key-12.asc"
+    fc-cache -rv
 
     ynh_print_info "microsoft fonts installed"
 }
 
 # Deactivate ttf-mscorefonts
 mscorefonts_deactivate(){
-	apt remove --purge ttf-mscorefonts-installer
-	fc-cache -rv
-	ynn_safe_rm /etc/apt/sources.list.d/collabora.list
+	apt-get remove ttf-mscorefonts-installer -y
+    fc-cache -rv
 
 	ynh_print_info "microsoft fonts removed"
 }
+
 
 # Reload templates
 coolconfig_update_system_template(){
