4acf59b796ed2f1a04d4b8572bb9e78ea196217d
eric_G <46165813+ericgaspar@users.noreply.github.com>
2026-02-26 14:00:03 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,7 +9,7 @@ mscorefonts_activate(){
     ynh_apt_install_dependencies_from_extra_repository --repo="deb http://deb.debian.org/debian bookworm contrib non-free-firmware" --package="ttf-mscorefonts-installer" --key="https://ftp-master.debian.org/keys/archive-key-12.asc"
     fc-cache -rv
 
-    ynh_print_info "microsoft fonts installed"
+    ynh_print_info "Microsoft fonts installed"
 }
 
 # Deactivate ttf-mscorefonts
@@ -17,7 +17,7 @@ mscorefonts_deactivate(){
 	apt purge ttf-mscorefonts-installer -y
     fc-cache -rv
 
-	ynh_print_info "microsoft fonts removed"
+	ynh_print_info "Microsoft fonts removed"
 }
 
 
